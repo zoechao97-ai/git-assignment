169ebedca04ac7be995d8a4836c96b475c086aa0
梓伊 赵 <ziyizhao@MacBook-Pro.local>
2026-05-06 16:54:54 -0400

    Update web scraping script

--- web-scraping.py
+++ web-scraping.py
@@ -1,27 +1,90 @@
+# Python script to scrape an article given the url of the article and store the extracted text in a file
+# Url: https://medium.com/@subashgandyer/papa-what-is-a-neural-network-c5e5cc427c7
+
 import os
 import requests
+import re
+import sys
+
+# Import BeautifulSoup library
 from bs4 import BeautifulSoup
 
-url = input("Enter Medium article URL: ")
 
-headers = {
-    "User-Agent": "Mozilla/5.0"
-}
+# function to get the html source text of the medium article
+def get_page():
+    global url
 
-response = requests.get(url, headers=headers)
-response.raise_for_status()
+    # Ask the user to input url
+    url = input("Enter url of a medium article: ")
 
-soup = BeautifulSoup(response.text, "html.parser")
+    # handling possible error
+    if not re.match(r'https?://medium.com/', url):
+        print('Please enter a valid website, or make sure it is a medium article')
+        sys.exit(1)
 
-paragraphs = soup.find_all("p")
-text = "\n".join([p.get_text(strip=True) for p in paragraphs])
+    # Send request to website
+    res = requests.get(url)
 
-folder_name = "Assignment_1"
-os.makedirs(folder_name, exist_ok=True)
+    res.raise_for_status()
 
-file_path = os.path.join(folder_name, "medium_article_text.txt")
+    soup = BeautifulSoup(res.text, 'html.parser')
 
-with open(file_path, "w", encoding="utf-8") as file:
-    file.write(text)
+    return soup
 
-print(f"Text saved to: {file_path}")
+
+# function to remove all the html tags and replace some with specific strings
+def clean(text):
+    rep = {"<br>": "\n", "<br/>": "\n", "<li>": "\n"}
+
+    rep = dict((re.escape(k), v) for k, v in rep.items())
+
+    pattern = re.compile("|".join(rep.keys()))
+
+    text = pattern.sub(lambda m: rep[re.escape(m.group(0))], text)
+
+    text = re.sub('\<(.*?)\>', '', text)
+
+    return text
+
+
+def collect_text(soup):
+
+    text = f'url: {url}\n\n'
+
+    para_text = soup.find_all('p')
+
+    print(f"paragraphs text = \n {para_text}")
+
+    for para in para_text:
+        text += f"{para.text}\n\n"
+
+    return text
+
+
+# function to save file in the current directory
+def save_file(text):
+
+    if not os.path.exists('./scraped_articles'):
+        os.mkdir('./scraped_articles')
+
+    name = url.split("/")[-1]
+
+    print(name)
+
+    fname = f'scraped_articles/{name}.txt'
+
+    # write text into file
+    with open(fname, 'w', encoding='utf-8') as f:
+        f.write(text)
+
+    print(f'File saved in directory {fname}')
+
+
+if __name__ == '__main__':
+
+    text = collect_text(get_page())
+
+    save_file(text)
+
+    # Instructions to Run this python code
+    # Give url as https://medium.com/@subashgandyer/papa-what-is-a-neural-network-c5e5cc427c7
